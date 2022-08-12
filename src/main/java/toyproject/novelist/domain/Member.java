3d618087eedd5021b2c6ccafa744ab50b4afc8d5
Yanggi Song <didrl741@gmail.com>
2022-08-12 13:08:44 +0900

    [add] H2 db 연결설정
    개발과정에서 쉽게 사용하기 위해 db를 h2로 연결했다. 나중에 다른걸로 바꿔끼우기만 하면 된다.

---
+++ src/main/java/toyproject/novelist/domain/Member.java
@@ -0,0 +1,22 @@
+package toyproject.novelist.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String loginId;
+
+    private String password;
+}
