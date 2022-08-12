dbc7daaeee988b2ed2259c48103264fff6fc44e8
Eungyu-code <Agyu625@gmail.com>
2022-08-12 21:46:02 +0900

    spring-security 기본 설정

---
+++ src/main/java/toyproject/novelist/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package toyproject.novelist;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/**").permitAll();
+
+        return http.build();
+    }
+}
