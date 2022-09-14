6b44a0cf2d92a864bc307bdd27255352afb226f8
Yanggi Song <didrl741@gmail.com>
2022-09-15 02:41:16 +0900

    [add] 초기 단어삽입 셋팅
    단어 후보가 많기때문에 액셀파일을 읽어서 반복문으로 단어를 삽입하고자 도입했다.
    고려사항 1. 액셀파일의 경로를 절대경로에서 상대경로로 바꿔야 한다.
    고려사항 2. 바꾼 상대경로는 배포 후 다시 수정해줘야 한다. (배포시 디렉토리구조가 바뀌기 때문)

--- src/main/java/toyproject/novelist/StartConfig.java
+++ src/main/java/toyproject/novelist/StartConfig.java
@@ -1,6 +1,12 @@
 package toyproject.novelist;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import toyproject.novelist.domain.word.TodayWords;
@@ -8,6 +14,9 @@ import toyproject.novelist.domain.word.Words;
 import toyproject.novelist.service.TodayWordsService;
 import toyproject.novelist.service.WordsService;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -18,14 +27,32 @@ public class StartConfig implements CommandLineRunner {
     private final WordsService wordsService;
     private final TodayWordsService todayWordsService;
 
+    private static final String EXT_XLS = "XLS";
+    private static final String EXT_XLSX = "XLSX";
+
+    Words words = new Words();
+
     // 서버가 시작될 때 생성
     @Override
     public void run(String... args) {
 
-        Words words = new Words();
-        for (int i = 0; i <= 20; i++) {
-            words.addWord("단어" + i);
+        //Words words = new Words();
+
+        // 현재 나의 로컬 절대경로로 설정. (상대경로로 바꿔야 함) + (배포시 또 수정사항 있음)
+        String path = "C:\\Users\\didrl\\OneDrive - 홍익대학교\\바탕 화면\\novelist\\src\\main\\resources\\static\\data\\words.xlsx";
+        File file = new File(path);
+
+        if( file.exists() ) {
+            System.out.println( "파일존재 확인!" );
         }
+
+        try {
+            readExcel(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
         wordsService.join(words);
 
@@ -34,5 +61,95 @@ public class StartConfig implements CommandLineRunner {
 
         todayWordsService.save(todayWords);
 
+    }
+
+    public  List<Map<String, String>> readExcel(String filePath ) throws IOException {
+
+        File file = new File(filePath);
+
+        // 확장자
+        String ext = FilenameUtils.getExtension( file.getName() );
+
+        List<Map<String, String>> resultMap = null;
+
+        if( EXT_XLS.equalsIgnoreCase( ext ) ) {
+            // resultMap = readExcelForXls(filePath);
+        }
+        else if( EXT_XLSX.equalsIgnoreCase( ext ) ) {
+            resultMap = readExcelForXlsx(filePath);
+        }
+
+        return resultMap;
+
+    }
+
+    @SuppressWarnings("resource")
+    private  List<Map<String, String>> readExcelForXlsx( String filePath ) throws IOException {
+
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+        int rows = sheet.getPhysicalNumberOfRows();
+        System.out.println("rows ====== " + rows);
+
+        List<Map<String, String>> resultList = new ArrayList<Map<String, String>>();
+
+        for( int rowindex = 0; rowindex < rows; rowindex++ ) {
+
+            // XSSFRow row = sheet.getRow(rowindex);
+            Row row = sheet.getRow(rowindex);
+
+            if( row == null ) {
+                break;
+                //continue;
+            }
+
+            int cells = row.getPhysicalNumberOfCells();
+
+            Map<String, String> resultMap = new HashMap<String, String>();
+
+            for( int columnindex = 0; columnindex <= cells; columnindex++ ) {
+
+                // XSSFCell cell = row.getCell(columnindex);
+                Cell cell = row.getCell(columnindex);
+
+                String value = "";
+
+                if( cell == null ) {
+                    continue;
+                }
+                else {
+
+                    switch (cell.getCellType()) {
+
+                        case STRING:
+                            value = cell.getStringCellValue();
+                            break;
+
+                        case BLANK:
+                            value = String.valueOf(cell.getBooleanCellValue());
+                            break;
+
+                        case ERROR:
+                            value = String.valueOf(cell.getErrorCellValue());
+
+                    }
+
+                }
+
+                resultMap.put( String.valueOf( columnindex ) , value );
+
+                words.addWord(value);
+                System.out.println("value ====> " + value);
+            }
+
+            resultList.add(resultMap);
+
+        }
+
+        return resultList;
     }
 }
